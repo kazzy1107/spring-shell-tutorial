abb761a4050c1f1c645212c8d0438313d60b0287
kazzy <kazzy1107@gmail.com>
2022-08-21 03:24:02 +0900

    Initial Commit

---
+++ src/main/java/com/github/kazzy1107/spring_shell_tutorial/SpringShellTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.github.kazzy1107.spring_shell_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShellTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShellTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/kazzy1107/spring_shell_tutorial/SpringShellTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kazzy1107.spring_shell_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShellTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
