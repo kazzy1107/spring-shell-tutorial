833a81e8ddf1ec79a657d0c4d7bf076d46c39cdb
Kazui Fujii <kazzy1107@gmail.com>
2022-08-21 08:48:31 +0900

    Added my first command

---
+++ src/main/java/com/github/kazzy1107/spring_shell_tutorial/MyCommands.java
@@ -0,0 +1,14 @@
+package com.github.kazzy1107.spring_shell_tutorial;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+@ShellComponent
+public class MyCommands {
+
+    @ShellMethod("Add two integers together.")
+    public int add(int a, int b) {
+        return a + b;
+    }
+    
+}
